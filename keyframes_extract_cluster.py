c63b2d7f9576f40945b180b0bb04561188936e99
dingjinyang <djy.dingjinyang@gmail.com>
2020-07-14 15:53:02 +0800

    improve keyframes_extract with cluster

--- keyframes_extract_cluster.py
+++ keyframes_extract_cluster.py
@@ -4,13 +4,37 @@ import numpy as np
 from typing import List
 from matplotlib import pyplot as plt
 
+# TODO
+# HSV颜空间下，图片相似度对比
+# 多进程计算
+
+# 相似度阈值
 threshold = float(0.9876543)
+# 打开当前文件夹下的 video.mp4 视频
+source = os.path.join(os.path.abspath('.'), 'video.mp4')
+target = os.path.join(os.path.dirname(source), 'frames_video')
 
 
-class FrameHist:
+class Time:
+    """帧时间"""
+
+    def __init__(self, milliseconds: float):
+        self.second, self.millisecond = divmod(milliseconds, 1000)
+        self.minute, self.second = divmod(self.second, 60)
+        self.hour, self.minute = divmod(self.minute, 60)
+
+    def __str__(self):
+        return f'{str(int(self.hour)) + "-" if self.hour else ""}' \
+               f'{str(int(self.minute)) + "-" if self.minute else ""}' \
+               f'{str(int(self.second)) + "-" if self.second else ""}' \
+               f'{int(self.millisecond)}'
+
+
+class Frame:
     """帧直方图"""
 
     def __init__(self, no: int, hist: List):
+        """帧序号、时间、直方图"""
         self.no = no
         self.hist = hist
 
@@ -18,7 +42,7 @@ class FrameHist:
 class FrameCluster:
     """帧聚类"""
 
-    def __init__(self, cluster: List[FrameHist], center: FrameHist):
+    def __init__(self, cluster: List[Frame], center: Frame):
         self.cluster = cluster
         self.center = center
 
@@ -29,6 +53,19 @@ class FrameCluster:
             for j in range(len(self.cluster)):
                 hist_sum[i] += self.cluster[j].hist[i]
         self.center.hist = [i / len(self.cluster) for i in hist_sum]
+
+    def keyframe_no(self) -> int:
+        """
+        聚类中与聚类中心相似度最高的
+        :return: 帧序号
+        """
+        no = self.cluster[0].no
+        max_similar = 0
+        for frame in self.cluster:
+            similar = similarity(frame.hist, self.center.hist)
+            if similar > max_similar:
+                max_similar, no = similar, frame.no
+        return no
 
 
 def similarity(frame1, frame2):
@@ -43,9 +80,14 @@ def similarity(frame1, frame2):
     return similar
 
 
-def handle_video_frames(source_path: str, target_path: str):
+def handle_video_frames(video_path: str) -> List[Frame]:
+    """
+    处理视频
+    :param video_path:
+    :return: 帧对象数组
+    """
     # 创建视频对象
-    cap = cv2.VideoCapture(source_path)
+    cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)  # 帧率
     frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)  # 帧数
     height, width = cap.get(cv2.CAP_PROP_FRAME_HEIGHT), cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # 帧高，宽
@@ -54,25 +96,20 @@ def handle_video_frames(source_path: str, target_path: str):
     print(f'Height of Video: {height}')
     print(f'Width of Video: {width}')
 
-    times = 1
-    frames, frame_hists = list(), list()
-    while True:
+    no = 1
+    frames = list()
 
-        # 读取视频帧
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        frames.append(frame)
+    # 读取视频帧
+    nex, frame = cap.read()
+    while nex:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # BGR -> HSV 转换颜色空间
         # 统计颜色直方图，[h,s,v]:[0,1,2] 分为 [12,5,5]份
         hist = cv2.calcHist([hsv], [0, 1, 2], None, [12, 5, 5], [0, 256, 0, 256, 0, 256])
-        print(type(hist))
         # numpy 3维数组扁平化
         flatten_hists = hist.flatten()
         # 求均值
         flatten_hists /= height * width
-        frame_hists.append(flatten_hists)
+        frames.append(Frame(no, flatten_hists))
 
         # 显示3个通道的颜色直方图
         # plt.plot(flatten_hists[:12], label=f'H_{times}', color='blue')
@@ -81,54 +118,72 @@ def handle_video_frames(source_path: str, target_path: str):
         # plt.legend(loc='best')
         # plt.xlim([0, 22])
 
-        times += 1
-    # plt.show()
+        no += 1
+        nex, frame = cap.read()
+
+        # plt.show()
 
     # 释放
+    cap.release()
+    return frames
 
-    # 聚类,第一个自成一类
-    clusters = list([FrameCluster([FrameHist(0, frame_hists[0])], FrameHist(0, frame_hists[0]))])
 
-    for no, hist in enumerate(frame_hists[1:]):
+def frames_cluster(frames: List[Frame]) -> List[FrameCluster]:
+    """
+    聚类
+    :param frames:
+    :return: 聚类数组
+    """
+    # 第一个自成一类
+    ret_clusters = [FrameCluster([frames[0]], Frame(0, frames[0].hist))]
+
+    for frame in frames[1:]:
         """
         将每一帧与已形成的聚类中心比较，取相似度最大值，其若小于阈值则自成一类，否则加入此类
         """
         max_ratio, clu_idx = 0, 0
-        for i, clu in enumerate(clusters):
-            sim_ratio = similarity(hist, clu.center.hist)
+        for i, clu in enumerate(ret_clusters):
+            sim_ratio = similarity(frame.hist, clu.center.hist)
             if sim_ratio > max_ratio:
                 max_ratio, clu_idx = sim_ratio, i
 
         # 最大相似度 与 阈值比较
         if max_ratio < threshold:
             """小于阈值，自成一类"""
-            clusters.append(FrameCluster([FrameHist(no, hist)], FrameHist(no, hist)))
+            ret_clusters.append(FrameCluster([frame], Frame(0, frame.hist)))
         else:
-            clusters[clu_idx].cluster.append(FrameHist(no, hist))
+            ret_clusters[clu_idx].cluster.append(frame)
             # 重新计算聚类中心
-            clusters[clu_idx].re_center()
-            clusters[clu_idx].center.no = no
+            ret_clusters[clu_idx].re_center()
 
-    # 按帧序号排序
-    clusters.sort(key=lambda x: x.center.no)
+    return ret_clusters
 
+
+def store_keyframe(video_path: str, target_path: str, frame_clusters: List[FrameCluster]):
+    """
+    从聚类中 保存视频关键帧
+    :return:
+    """
+    # 创建视频对象
+    cap = cv2.VideoCapture(video_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 帧数
+
+    # video_path关键帧序号
+    nos = set([cluster.keyframe_no() for cluster in frame_clusters])
     if not os.path.exists(target_path):
         os.mkdir(target_path)
 
-    # 保存
-    # for clu in clusters:
-    # cv2.imwrite(f'{target_path}/{clu.center.no}.jpg', frames[clu.center.no])
-
-    cap.release()
+    no = 1
+    # 读取视频帧
+    nex, frame = cap.read()
+    while nex:
+        if no in nos:
+            cv2.imwrite(f'{target_path}/{no}-{frame_count}_{Time(cap.get(cv2.CAP_PROP_POS_MSEC))}.jpg', frame)
+        no += 1
+        nex, frame = cap.read()
 
 
 if __name__ == '__main__':
-    # 打开当前文件夹下的 video.mp4 视频
-    source = os.path.join(os.path.abspath('.'), 'video.mp4')
-    target = os.path.join(os.path.dirname(source), 'frames_video')
-    handle_video_frames(source, target)
-    # video_writer = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 25, (464, 848))  # (1360,480)为视频大小
-    # print(os.listdir(target))
-    # for img_name in os.listdir(target):
-    #     video_writer.write(cv2.imread(f'{target}/{img_name}'))
-    # video_writer.release()
+    frames = handle_video_frames(source)
+    clusters = frames_cluster(frames)
+    store_keyframe(source, target, clusters)
